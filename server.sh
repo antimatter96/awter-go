b647d4091d053cdc3ddccaa8e91be4077b11eabc
Arpit Jain <antimatter96@users.noreply.github.com>
2020-07-01 03:37:20 +0530

    Update docker setup

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Building"
-go build -o awter-go -race -ldflags "-X 'main.build=$(date +%Y-%m-%d\ %H:%M)'"
+go build -o awter-go -race -ldflags "-X 'main.build=$(date +%Y-%m-%d_%H:%M)'"
 echo "Built"
 # ./awter-go --port=9005 --mysqlURL="root:661996@/awter_db_test?parseTime=true"
-./awter-go --port=9005 --mysqlURL=""
+./awter-go --port 9005 --mysqlURL ""
