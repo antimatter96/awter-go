b03921f01221867da63a7d62edac2776e39db324
Arpit Jain <antimatter96@users.noreply.github.com>
2020-07-05 00:38:53 +0530

    Use partial templates

--- server/templates.go
+++ server/templates.go
@@ -3,7 +3,25 @@ package server
 import "html/template"
 
 func (server *Server) parseTemplates(templatePath string) {
-	server.shortnerTemplate = template.Must(template.ParseFiles(templatePath + "/shortner.html"))
-	server.createdTemplate = template.Must(template.ParseFiles(templatePath + "/created.html"))
-	server.elongateTemplate = template.Must(template.ParseFiles(templatePath + "/elongate.html"))
+	server.shortnerTemplate = template.Must(template.ParseFiles(
+		[]string{
+			templatePath + "/shortner.html",
+			templatePath + "/_heading.html",
+			templatePath + "/_showHidePassword.html",
+		}...,
+	))
+	server.createdTemplate = template.Must(template.ParseFiles(
+		[]string{
+			templatePath + "/created.html",
+			templatePath + "/_heading.html",
+			templatePath + "/_showHidePassword.html",
+		}...,
+	))
+	server.elongateTemplate = template.Must(template.ParseFiles(
+		[]string{
+			templatePath + "/elongate.html",
+			templatePath + "/_heading.html",
+			templatePath + "/_showHidePassword.html",
+		}...,
+	))
 }
